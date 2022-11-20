3c61ca955aa8c24010cc492a70215b90df8a7e52
larissa-abboud <lariabboud@gmail.com>
2022-11-20 22:45:55 +0200

    updating apis

--- Backend/logInVerificationApi.php
+++ Backend/logInVerificationApi.php
@@ -8,4 +8,21 @@ include("connect.php");
  * check in database if exists
  * return true if exist
  */
+$username = $_GET["username"] ??"" ;
+$password = $_GET["password"] ??"";
 
+
+
+
+
+$query = $mysqli->prepare("Select username,password from users");
+$query->bind_param("ss", $username ,$password);
+$query->execute();
+
+$array = $query->get_result();
+$response = [];
+while($user = $array->fetch_assoc()){
+    $response[] = $user;
+}
+
+echo json_encode($response);
